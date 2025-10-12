2a5af4d916811654f00d677233ad639372890190
xiao66778 <kghq006@gmail.com>
2025-10-12 21:07:26 +0800

    Update xiaoHub.lua

--- xiaoHub.lua
+++ xiaoHub.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/8947b1dacc10909ebcba4ffcbb81caf2c01488bcbbdaa01f44e109b553f0e8c8/download"))()
+loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/17061e9d12c63455ca8f32e4c130b1236bbad2a28c65ed881199074afbc1cb2b/download"))()
